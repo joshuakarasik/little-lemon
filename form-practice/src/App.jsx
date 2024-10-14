ea7cf057bf3946ba7af1955646beafdfb98722e3
Joshua Karasik <joshuakarasik@gmail.com>
2024-10-14 10:46:24 -0700

    fomr

--- form-practice/src/App.jsx
+++ form-practice/src/App.jsx
@@ -1,21 +1,35 @@
 import { useState } from "react";
-import { useRef } from "react";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useState("");
+  const [name, setName] = useState("");
 
-  const handleChange = (e) => {
-    setValue(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setName("");
+    console.log("form submitted!");
   };
 
   return (
     <>
-      <form action="form">
-        <input
-          type="text"
-          value={value}
-        />
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button
+            disabled={!name}
+            type="submit"
+          >
+            Submit
+          </button>
+        </fieldset>
       </form>
     </>
   );
